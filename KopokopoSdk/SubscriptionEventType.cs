2323b111b4b4dab6ed336aec422e1723d5a81c9a
Gabriel Odero <gabupostiga.gdco@gmail.com>
2022-05-25 02:32:39 +0300

    Event subscription type added

---
+++ KopokopoSdk/SubscriptionEventType.cs
@@ -0,0 +1,12 @@
+﻿namespace KopokopoSdk
+{
+    public static class SubscriptionEventType
+    {
+        public const string BuyGoodsTransactionReceived = "buygoods_transaction_received";
+        public const string BuyGoodsTransactionReversed = "buygoods_transaction_reversed";
+        public const string B2BTransactionReceived = "b2b_transaction_received";
+        public const string M2MTransactionReceived = "m2m_transaction_received";
+        public const string SettlementTransferCompleted = "settlement_transfer_completed";
+        public const string CustomerCreated = "customer_created";
+    }
+}
