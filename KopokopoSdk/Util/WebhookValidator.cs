eecc43e6a908891234243380138cc7310932d9a0
Gabriel Odero <gabupostiga.gdco@gmail.com>
2022-05-25 23:15:27 +0300

    Webhook validator added

---
+++ KopokopoSdk/Util/WebhookValidator.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace KopokopoSdk.Util
+{
+    public static class WebhookSignatureUtil
+    {
+        public static bool ValidateKopokopoSignature(string requestBodyPayload, string apiKey, string webhookSignature)
+        {
+            if (string.IsNullOrWhiteSpace(webhookSignature))
+            {
+                throw new ArgumentNullException(nameof(webhookSignature));
+            }
+            else
+            {
+                // Validating signature
+                var keyBytes = Encoding.ASCII.GetBytes(apiKey);
+                var payLoadDataBytes = Encoding.ASCII.GetBytes(requestBodyPayload);
+
+                using var hmac = new HMACSHA256(keyBytes);
+                var hmacBytes = hmac.ComputeHash(payLoadDataBytes);
+
+                var createSignature = BitConverter.ToString(hmacBytes).Replace("-", "").ToLower();
+
+                if (webhookSignature.Equals(createSignature))
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+        }
+    }
+}
